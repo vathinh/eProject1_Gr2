c1003a90de388e9e8b86a01b2ecea4d5c22a66b0
vathinh <tv.thinh2000@gmail.com>
2022-04-21 00:12:45 +0700

    darkmode

--- Eprojct1/JS/main.js
+++ Eprojct1/JS/main.js
@@ -21,3 +21,7 @@
         var element = document.body;
         element.classList.toggle("dark-mode");
         }
+        function myFunction() {
+            var element = document.body;
+            element.classList.toggle("dark-mode");
+         }
